11d3431219766df1bc9b83e04e4f720e2ef0258a
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-03-24 21:30:04 +0000

    Completed merge_sort recursive solution and fibonacci Rubocop amendments

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,29 +1,34 @@
 # Gives an array of Fibonacci numbers n long (zero indexed)
-def fibs(n, result = [])
-  return [0] if n == 0
-  return [0, 1] if n == 1
+def fibs(number, result = [])
+  return [0] if number.zero?
+
+  return [0, 1] if number == 1
+
   result = [0, 1]
-  (n - 1).times do
+  (number - 1).times do
     result << result[-1] + result[-2]
   end
   result
 end
 
 # Gives an array of Fibonacci numbers n long (zero indexed) using recursion
-def fibs_rec(n)
- return [0] if n == 0
- return [0, 1] if n == 1
- result = fibs_rec(n - 1)
- result << result[-1] + result[-2]
+def fibs_rec(number)
+  return [0] if number.zero?
+
+  return [0, 1] if number == 1
+
+  result = fibs_rec(number - 1)
+  result << result[-1] + result[-2]
 end
 
 # Gives the Fibonacci number in a given position n using recursion
-def fib(n)
-  if n == 0
+def fib(number)
+  case number
+  when 0
     0
-  elsif n == 1
+  when 1
     1
   else
-    fib(n-1) + fib(n-2)
+    fib(number - 1) + fib(number - 2)
   end
 end
